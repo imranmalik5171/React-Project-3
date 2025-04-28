5f00cfde0319189d2a8b11f78de33ba186996bb7
imranmalik5171 <imran8malik4567@gmail.com>
2025-04-28 23:08:53 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Nav from "./Nav";
+import Hero from "./Hero";
+import Expertise from "./Expertise";
+import Contact from "./Contact";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Expertise />
+      <Contact />
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/Expertise.jsx
@@ -0,0 +1,86 @@
+
+
+function Expertise () {
+    return(
+        <div class="expertise-section">
+        <h2>AREAS OF EXPERTISE</h2>
+        <h5>
+          Exploring the limitless possibilities of cutting-edge technology <br />
+          with our proficient team.
+        </h5>
+      
+        <div class="expertise-grid">
+      
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+      
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+      
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+      
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+      
+          <div class="expertise-card">
+            <h4>MACHINE LEARNING</h4>
+            <p>Lorem ipsum is simply dummy text of the printing and typesetting industry. Lorem ipsum has.</p>
+            <button>VIEW MORE</button>
+          </div>
+        </div>
+      
+        <button class="learn-more-btn">LEARN MORE</button>
+      </div>
+      
+    )
+}
+
+export default Expertise;
